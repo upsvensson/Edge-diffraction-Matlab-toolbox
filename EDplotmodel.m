c12df88912af3c3f23cd2658947605eba79c1bd3
Peter Svensson <peter.svensson@ntnu.no>
2021-03-14 10:38:20 +0100

    Added the output of handles to all figure parts

--- EDplotmodel.m
+++ EDplotmodel.m
@@ -1,4 +1,4 @@
-function EDplotmodel(eddatafile,varargin)
+function plothandles = EDplotmodel(eddatafile,varargin);
 % EDplotmodel - Plots a model which is given in an eddatafile.
 %
 % Input parameters:
@@ -32,14 +32,34 @@ function EDplotmodel(eddatafile,varargin)
 %                           If the text-string 'edgenumbers' is given,
 %                           followed by a vector of integers, only those
 %                           edge numbers are plotted with a solid line.
-%                           The others are plotted with dashed lines.                           
-%
+%                           The others are plotted with dashed lines. 
+%   'figurewindow' a desired figure number. If not specified, figure number
+%   1 will be used.
+% 
+% Output parameters:
+%   plothandles     A struct with fields that give handles to the various
+%                   parts of the figure:
+%                   .parent   To set the size of the axis values, change
+%                             the parameter 'FontSize' of this figure
+%                             handle etc.
+%                   .sources
+%                   .receivers
+%                   .edges        A list, one value for each edge
+%                   .edgecircles  A list, one value for each edge
+%                   .edgenumbers  A list, one value for each edge
+%                   .conumbers    A list, one value for each corner
+%                   .nvecs        A list, one value for each nvec
+%                   .nveccircles  A list, one value for each nvec
+%                   .planenumbers A list, one value for each plane
+% 
 % Sources and Rdata.receivers are taken from an sdatafile and an rdatafile, the file name of
 % which is assumed to be similar to the eddatafile.
 %
 % Uses functions EDstrpend, EDstrpblnk
 %
-% Peter Svensson (peter.svensson@ntnu.no) 3 June 2020
+% Peter Svensson (peter.svensson@ntnu.no) 14 March 2021
+% 
+% plothandles = EDplotmodel(eddatafile,varargin);
 
 % 29 April 2016 Completed the use of structs planedata and edgedata
 % 1 Nov 2017 Added EDstrpblnk for the sdata etc file names. Also skipped the
@@ -52,7 +72,8 @@ function EDplotmodel(eddatafile,varargin)
 % worked??). Also removed the little circles on edges if edgenumbers are
 % not plotted.
 % 3 June 2020 Fixed a bug: folder names with spaces can be handled now
-
+% 14 March 2021 Added plothandles as output parameter, and figurewindow as
+% optional input parameter
 
 p = inputParser;
 
@@ -60,6 +81,7 @@ p.addOptional('plotoptions',0,@isscalar);
 p.addParamValue('sounumbers',[],@isnumeric);
 p.addParamValue('recnumbers',[],@isnumeric);
 p.addParamValue('edgenumbers',[],@isnumeric);
+p.addParamValue('figurewindow',1,@isscalar);
 
 p.parse(varargin{:})
 inputs = p.Results;
@@ -68,7 +90,16 @@ plotoptions = inputs.plotoptions;
 sounumbers = inputs.sounumbers;
 recnumbers = inputs.recnumbers;
 edgenumbers = inputs.edgenumbers;
- 
+figurewindow = inputs.figurewindow;
+
+%--------------------------------------------------------------
+% Output struct
+
+plothandles = struct('parent',[],...
+    'sources',[],'receivers',[],'edges',[],'edgecircles',[],...
+    'edgenumbers',[],'conumbers',[],'nvecs',[],'nveccircles',[],...
+    'planenumbers',[]);
+                     
 %--------------------------------------------------------------
 % Read the eddatafile
 
@@ -88,22 +119,6 @@ disp(['eddatafile is ',eddatafilepath,filesep,eddatafile])
 
 %--------------------------------------------------------------
 % Read and decode the second input parameter
-
-% if nargin == 1
-%     if isstr(eddatafile) ~= 1
-%         plotoptions = eddatafile;
-%     else
-%         plotoptions = 0;    
-%     end
-% elseif nargin == 0
-%     plotoptions = 0;    
-% end
-% 
-% if nargin < 3
-%     plotoptions2 = [];
-% else
-%     nplotoptions2 = size(plotoptions2,1);
-% end
 
 plotsources = bitget(plotoptions,1);
 plotreceivers = bitget(plotoptions,2);
@@ -174,21 +189,22 @@ end
 linemarkertype = ['k- ';'k--'];
 linewidthvalues = [2 1];
 
-% figure(1)
-hold off
+figure(figurewindow)
+clf(figurewindow)
 for ii = 1:nedges
 	co1 = edgedata.edgecorners(ii,1);
 	co2 = edgedata.edgecorners(ii,2);
 	iv = [co1;co2];
-	h = plot3(planedata.corners(iv,1),planedata.corners(iv,2),planedata.corners(iv,3),linemarkertype(linemarker(ii),:));
-    set(h,'LineWidth',linewidthvalues(linemarker(ii)));
+	plothandles.edges(ii) = plot3(planedata.corners(iv,1),planedata.corners(iv,2),planedata.corners(iv,3),linemarkertype(linemarker(ii),:));
+    set(plothandles.edges(ii),'LineWidth',linewidthvalues(linemarker(ii)));
     if ii ==1
         view(viewpos)
         hold on
+        plothandles.parent = get(plothandles.edges(1),'Parent');
     end
     if plotednumbers
         costart = planedata.corners(co1,:) + 0.1*(planedata.corners(co2,:)-planedata.corners(co1,:));
-        h = plot3(costart(1),costart(2),costart(3),'ko');    
+        plothandles.edgecircles(ii) = plot3(costart(1),costart(2),costart(3),'ko');    
     end
 end
 
@@ -198,8 +214,8 @@ if plotnvecs
         
         endpoint = midpoint + planedata.planeeqs(ii,1:3);
         bothpoints = [midpoint;endpoint];
-        plot3(bothpoints(:,1),bothpoints(:,2),bothpoints(:,3));
-        plot3(midpoint(1),midpoint(2),midpoint(3),'ro');
+        plothandles.nvecs(ii) = plot3(bothpoints(:,1),bothpoints(:,2),bothpoints(:,3));
+        plothandles.nveccircles(ii) = plot3(midpoint(1),midpoint(2),midpoint(3),'ro');
 	end
 end
 
@@ -208,11 +224,11 @@ if plotplnumbers
         midpoint = mean(planedata.corners(planedata.planecorners(ii,1:planedata.ncornersperplanevec(ii)),:));
         endpoint = midpoint + planedata.planeeqs(ii,1:3)*0.1;
         if plotplCADnumbers == 1
-            h = text(endpoint(1),endpoint(2),endpoint(3),['p',int2str(planenumbers(ii))]);
-            set(h,'FontSize',14);
+            plothandles.planenumbers(ii) = text(endpoint(1),endpoint(2),endpoint(3),['p',int2str(planenumbers(ii))]);
+            set(plothandles.planenumbers(ii),'FontSize',14);
         else
-            h = text(endpoint(1),endpoint(2),endpoint(3),['p',int2str(ii)]);
-            set(h,'FontSize',14);
+            plothandles.planenumbers(ii) = text(endpoint(1),endpoint(2),endpoint(3),['p',int2str(ii)]);
+            set(plothandles.planenumbers(ii),'FontSize',14);
         end
     
 	end
@@ -221,19 +237,19 @@ end
 if plotednumbers
     for ii = 1:nedges
         midpoint = mean(planedata.corners(edgedata.edgecorners(ii,1:2),:));
-        h = text(midpoint(1),midpoint(2),midpoint(3),['e',int2str(ii)]);
-            set(h,'FontSize',18);
+        plothandles.edgenumbers(ii) = text(midpoint(1),midpoint(2),midpoint(3),['e',int2str(ii)]);
+            set(plothandles.edgenumbers(ii),'FontSize',18);
     end
 end
 
 if plotconumbers
     for ii = 1:ncorners
         if plotcoCADnumbers == 1
-            h = text(planedata.corners(ii,1),planedata.corners(ii,2),planedata.corners(ii,3),['c',int2str(cornernumbers(ii))]); 
-            set(h,'FontSize',18);
+            plothandles.conumbers(ii) = text(planedata.corners(ii,1),planedata.corners(ii,2),planedata.corners(ii,3),['c',int2str(cornernumbers(ii))]); 
+            set(plothandles.conumbers(ii),'FontSize',18);
         else
-            h = text(planedata.corners(ii,1),planedata.corners(ii,2),planedata.corners(ii,3),['c',int2str(ii)]);    
-            set(h,'FontSize',18);
+            plothandles.conumbers(ii) = text(planedata.corners(ii,1),planedata.corners(ii,2),planedata.corners(ii,3),['c',int2str(ii)]);    
+            set(plothandles.conumbers(ii),'FontSize',18);
         end
     end
     
@@ -241,18 +257,20 @@ end
 
 if plotsources == 1
     if isempty(sounumbers)
-        plot3(Sdata.sources(:,1),Sdata.sources(:,2),Sdata.sources(:,3),'*');
+        plothandles.sources = plot3(Sdata.sources(:,1),Sdata.sources(:,2),Sdata.sources(:,3),'*');
     else
-        plot3(sources(sounumbers,1),sources(sounumbers,2),sources(sounumbers,3),'*')
+        plothandles.sources = plot3(sources(sounumbers,1),sources(sounumbers,2),sources(sounumbers,3),'*');       
     end
+    set(plothandles.sources,'LineWidth',3)
 end
 
 if plotreceivers == 1
     if isempty(recnumbers)
-        plot3(Rdata.receivers(:,1),Rdata.receivers(:,2),Rdata.receivers(:,3),'ro');
+        plothandles.receivers = plot3(Rdata.receivers(:,1),Rdata.receivers(:,2),Rdata.receivers(:,3),'ro');
     else
-        plot3(Rdata.receivers(recnumbers,1),Rdata.receivers(recnumbers,2),Rdata.receivers(recnumbers,3),'ro');
+        plothandles.receivers = plot3(Rdata.receivers(recnumbers,1),Rdata.receivers(recnumbers,2),Rdata.receivers(recnumbers,3),'ro');
     end
+    set(plothandles.receivers,'LineWidth',3)
 end
 
 xlabel('x')
